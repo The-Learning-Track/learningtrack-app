35675ce6acd74d99cb359d08931bc199457741d1
System Administrator <root@mary008-0103-dhcp245.bu.edu>
2012-10-11 12:37:47 -0400

    Initial Commit

---
+++ TheLearningTrack/TheLearningTrack/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  TheLearningTrack
+//
+//  Created by Senior Design on 10/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+//@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+//@property (nonatomic, retain) IBOutlet UIViewController *viewController;
+
+@end
---
+++ TheLearningTrack/TheLearningTrack/ViewController.h
@@ -0,0 +1,29 @@
+//
+//  ViewController.h
+//  TheLearningTrack
+//
+//  Created by Senior Design on 10/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    
+    IBOutlet UITextField    *username;
+    IBOutlet UITextField    *password;
+    IBOutlet UILabel        *testLabel;
+    
+}
+
+//Each property must be synthesized in the .m file
+@property (retain, nonatomic) UILabel *testLabel;   
+@property (retain, nonatomic) UITextField *username;
+@property (retain, nonatomic) UITextField *password;
+
+//Name of the action: What initiates that action 
+-(IBAction)loginButtonPressed:(id)sender;
+-(IBAction)hideKeyboard:(id)sender; 
+//-(IBAction)alert;
+
+@end
